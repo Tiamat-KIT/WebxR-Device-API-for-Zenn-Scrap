a7cc44c72e21434fed76351a62f78669e4537b0e
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-05-06 03:30:10 +0900

    window読み込み時に実行されるようにした

--- vite-webxr/src/main.ts
+++ vite-webxr/src/main.ts
@@ -7,15 +7,22 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
-const xr = window.navigator.xr;
-if(xr !== undefined){
-  xr.isSessionSupported("immersive-ar").then((isSupported) => {
-    if(isSupported){
-      xr.requestSession("immersive-ar").then((session) => {
-        console.log("Session Started!")
-      })
-    }
-  })
-} else {
-  console.error("Can't Used WebxR")
+window.onload = () => {
+  const xRSessionOption: XRSessionInit = {
+    optionalFeatures: ["hand-tracking"],
+  }
+  
+  const xr = window.navigator.xr;
+  if(xr !== undefined){
+    xr.isSessionSupported("immersive-ar").then((isSupported) => {
+      if(isSupported){
+        xr.requestSession("immersive-ar",xRSessionOption).then((session) => {
+          
+          console.log("Session Started!")
+        })
+      }
+    })
+  } else {
+    console.error("Can't Used WebxR")
+  }
 }
